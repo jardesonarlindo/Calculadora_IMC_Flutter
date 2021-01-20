bc9caf7ccfeca28a73b1915a0cc80e23226e5005
Jardeson Arlindo <jardesonaraujoarlindo@gmail.com>
2021-01-20 10:42:30 -0300

    Aula 02 Concluida

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home()
-  ));
+  runApp(MaterialApp(home: Home()));
 }
-
 
 class Home extends StatefulWidget {
   @override
@@ -20,9 +17,36 @@ class _HomeState extends State<Home> {
         title: Text("Calculadora de IMC"),
         centerTitle: true,
         backgroundColor: Colors.green,
-        actions: <Widget> [ IconButton(icon: Icon(Icons.refresh), onPressed: (){},) ],
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Peso (kg)",
+                labelStyle: TextStyle(color: Colors.green)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Altura (cm)",
+                labelStyle: TextStyle(color: Colors.green)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+        ],
       ),
     );
   }
 }
-
